59dcd3d2145f2e4561223af64c901a77fbf34aa6
Benny Rigler <rigbmail@gmail.com>
2025-01-20 12:39:37 +0000

    finaly Builds again

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,11 @@
 **/
 
 #include <memory>
-
 #include "xeus/xkernel.hpp"
 #include "xeus/xkernel_configuration.hpp"
+
 #include "xeus-zmq/xserver_zmq.hpp"
+#include "xeus-zmq/xzmq_context.hpp"
 
 #include "custom_interpreter.hpp"
 
@@ -30,14 +31,15 @@ int main(int argc, char* argv[]) {
     xeus::xconfiguration config = xeus::load_configuration(file_name);
 
     // Prepare Xeus' context
-    auto context = xeus::make_context<zmq::context_t>();
+    //auto context = xeus::make_context<zmq::context_t>();
+    auto context =  xeus::make_zmq_context();
 
     // Create interpreter instance
     using interpreter_ptr = std::unique_ptr<bscript::custom_interpreter>;
     interpreter_ptr interpreter = interpreter_ptr(new bscript::custom_interpreter());
 
     // Create kernel instance and start it
-    xeus::xkernel kernel(config, xeus::get_user_name(), std::move(context), std::move(interpreter), xeus::make_xserver_zmq);
+    xeus::xkernel kernel(config, xeus::get_user_name(), std::move(context), std::move(interpreter),xeus::make_xserver_default);
     kernel.start();
 
     return 0;
